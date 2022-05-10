d50c51a2503f3c7b5539d4d59b5edaf38fcb477d
Menuka Pinto <instafeed9999@gmail.com>
2022-05-11 02:47:26 +0530

    Insert CRUD Done

--- src/Components/Stock/stockForm.js
+++ src/Components/Stock/stockForm.js
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../App.css';
 import { Table, Container, Navbar, Row, Col, Nav, Button, Form, InputGroup } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { async } from '@firebase/util';
+import StockDataService from '../../Util/StockContext';
+
+// https://www.youtube.com/watch?v=cXWDQhzC3do
 
 function stockForm() {
+
+    // States
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [quantity, setQuantity] = useState("");
+    const [category, setCategory] = useState("");
+    const [brand, setBrand] = useState("");
+    const [status, setStatus] = useState("");
+
+    // ---------------- Handling Form Submit START ----------------
+    const handleSubmit =   async (e) => {
+
+        e.preventDefault();  // prevents the page refreshing on submit
+
+        const newStock = {
+            name,
+            description,
+            quantity,
+            category,
+            brand,
+            status
+        };
+        console.log(newStock);
+
+        try {
+            await StockDataService.addStocks(newStock);
+
+        } catch (err) {
+            console.log("There is an error")
+        }
+
+
+        // Emptying the form fields
+        setName("");
+        setDescription("");
+        setQuantity("");
+        setCategory("");
+        setBrand("");
+        setStatus("");
+
+    };
+    // ---------------- Handling Form Submit END ----------------
+
+
+
+
     return (
 
         <div className='page vertically-center'>
@@ -15,23 +65,12 @@ function stockForm() {
                 </div>
 
                 {/* ---------- Stock Form START ---------- */}
-                <Form className='rounded p-4 p-sm-4 login-form'>
+                <Form onSubmit={handleSubmit} className='rounded p-4 p-sm-4 login-form'>
                     <h1 className='font-weight-bold text-center pb-4 login-form-header'>
                         Track Stock Form
                     </h1>
 
                     <Row>
-                        <Col>
-                            <Form.Group className="mb-3" controlId="formProductId">
-                                <Form.Label>Product ID</Form.Label>
-                                <InputGroup>
-                                    <Form.Control
-                                        type="text"
-                                        placeholder="Product ID"
-                                    />
-                                </InputGroup>
-                            </Form.Group>
-                        </Col>
                         <Col>
                             <Form.Group className="mb-4" controlId="formProductName">
                                 <Form.Label>Product Name</Form.Label>
@@ -39,21 +78,42 @@ function stockForm() {
                                     <Form.Control
                                         type="text"
                                         placeholder="Product Name"
+                                        value={name}
+                                        onChange={(e) => setName(e.target.value)}
                                     />
                                 </InputGroup>
                             </Form.Group>
                         </Col>
                     </Row>
 
-                    <Form.Group className="mb-4" controlId="formProductDesc">
-                        <Form.Label>Description</Form.Label>
-                        <InputGroup>
-                            <Form.Control
-                                type="text"
-                                placeholder="Description"
-                            />
-                        </InputGroup>
-                    </Form.Group>
+                    <Row>
+                        <Col>
+                            <Form.Group className="mb-4" controlId="formProductDesc">
+                                <Form.Label>Description</Form.Label>
+                                <InputGroup>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Description"
+                                        value={description}
+                                        onChange={(e) => setDescription(e.target.value)}
+                                    />
+                                </InputGroup>
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group className="mb-4" controlId="formProductQuantity">
+                                <Form.Label>Quantity</Form.Label>
+                                <InputGroup>
+                                    <Form.Control
+                                        type="number"
+                                        placeholder="Quantity"
+                                        value={quantity}
+                                        onChange={(e) => setQuantity(e.target.value)}
+                                    />
+                                </InputGroup>
+                            </Form.Group>
+                        </Col>
+                    </Row>
 
                     <Row>
                         <Col>
@@ -62,6 +122,8 @@ function stockForm() {
                                 <Form.Label>Category</Form.Label>
                                 <Form.Select
                                     aria-label="Categories"
+                                    value={category}
+                                    onChange={(e) => setCategory(e.target.value)}
                                 >
                                     <option value=""></option>
                                     <option value="Mobile Phones">Mobile Phones</option>
@@ -78,6 +140,8 @@ function stockForm() {
                                     <Form.Control
                                         type="text"
                                         placeholder="Brand"
+                                        value={brand}
+                                        onChange={(e) => setBrand(e.target.value)}
                                     />
                                 </InputGroup>
                             </Form.Group>
@@ -87,7 +151,9 @@ function stockForm() {
                             <Form.Group className="mb-3" controlId="formProductStatus">
                                 <Form.Label>Status</Form.Label>
                                 <Form.Select
-                                    aria-label="Statuss"
+                                    aria-label="Status"
+                                    value={status}
+                                    onChange={(e) => setStatus(e.target.value)}
                                 >
                                     <option value=""></option>
                                     <option value="High">High</option>
--- src/Components/Stock/stockLevel.js
+++ src/Components/Stock/stockLevel.js
@@ -2,8 +2,30 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../App.css';
 import { Table, Container, Navbar, Row, Col, Nav, Button, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import StockDataService from '../../Util/StockContext';
+import { useState } from 'react';
 
 function stockLevel() {
+
+    // States
+    const [stocks, setStocks] = useState([]);
+
+
+
+    // Fetch all stocks
+    useEffect(() => {
+        getStocks();
+        console.log("Getting all books from useEffect");
+    }, []);
+
+    const getStocks = async () => {
+        const data = await StockDataService.getAllStocks();
+        // setlastDoc(data.docs[data.docs.length - 1])
+        console.log(data.docs);
+        // setBooks(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+
 
     return (
 
@@ -21,9 +43,9 @@ function stockLevel() {
                         <thead>
                             <tr className="text-center p-2">
                                 <th>#</th>
-                                <th>Product ID</th>
                                 <th>Product Name</th>
                                 <th>Description</th>
+                                <th>Quantity</th>
                                 <th>Category</th>
                                 <th>Brand</th>
                                 <th>Status</th>
@@ -33,9 +55,9 @@ function stockLevel() {
                         <tbody>
                             <tr className="text-center p-2">
                                 <td>1</td>
-                                <td>AP3455</td>
                                 <td>Iphone 12 Pro Max</td>
                                 <td>12 GB RAM and 15 Mega Pixel Camera</td>
+                                <td>24</td>
                                 <td>Mobile Phones</td>
                                 <td>Apple</td>
                                 <td>High</td>
@@ -46,9 +68,9 @@ function stockLevel() {
                             </tr>
                             <tr className="text-center p-2">
                                 <td>2</td>
-                                <td>TG32424</td>
                                 <td>40W Grinder</td>
                                 <td>High Power hand grinder</td>
+                                <td>14</td>
                                 <td>Tools</td>
                                 <td>Hitachi</td>
                                 <td>Low</td>
